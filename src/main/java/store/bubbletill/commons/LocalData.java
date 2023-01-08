8cff6e4a0878e85e31343c52016d6a281423106d
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2023-01-08 11:42:06 +0000

    Phasing out local server

---
+++ src/main/java/store/bubbletill/commons/LocalData.java
@@ -0,0 +1,20 @@
+package store.bubbletill.commons;
+
+public class LocalData {
+
+    private int storeno;
+    private int regno;
+    private String token;
+    private String backend;
+    private String backoffice;
+
+    public int getStore() { return storeno; }
+
+    public int getReg() { return regno; }
+
+    public String getToken() { return token; }
+
+    public String getBackend() { return backend; }
+
+    public String getBackoffice() { return backoffice; }
+}
